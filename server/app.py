4bb9fe9a8d0e77bfc75dce81a80ed98f789b8f7e
Francis <franciskarani15@gmail.com>
2025-10-21 22:39:00 +0300

    creates the first endpoint

--- server/app.py
+++ server/app.py
@@ -4,4 +4,16 @@ from flask_cors import CORS
 from flask_restful import Resource, Api
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///notify.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+api=Api(app)
+
+class Welcome(Resource):
+    def get(self):
+        return "hi"
+api.add_resource(Welcome, "/")
+
+if __name__ =="__main__":
+    app.run(debug=True)
 

4f121989756da456ce175ef9240c59d1dbf5fab9
Francis <franciskarani15@gmail.com>
2025-10-22 11:36:32 +0300

    adds components for add courses

---
+++ client/src/pages/PostCourses.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PostCourses() {
+  return (
+      <div>
+          <form action="">
+              
+          </form>
+      
+    </div>
+  )
+}
